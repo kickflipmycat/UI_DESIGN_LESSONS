5a4932e761527e6c4c4a6090419f5ba77545da18
kickflipmycat <jamiechadwick1986@hotmail.com>
2024-08-08 19:19:32 +0930

    pizzaOrder

---
+++ Session02Exercises/Session02Exercises/PizzaOrder.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Session02Exercises
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PizzaOrder : Page
+    {
+        public PizzaOrder()
+        {
+            this.InitializeComponent();
+        }
+
+        private void orderButton_Click(object sender, RoutedEventArgs e)
+        {
+            messageTextBlock.Text = "JRs Pizza order submitted, your order will be ready in 30 minutes!";
+        }
+    }
+}
